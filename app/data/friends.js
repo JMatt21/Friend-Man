22a46b33d9d989413cdedd2a49bfea5144a559cd
JustMatt <MatthewBertrandt@gmail.com>
2018-08-02 09:59:23 -0700

    added routing and ajax logic

---
+++ app/data/friends.js
@@ -0,0 +1,28 @@
+let friendsArray = [
+    {
+        "name": "Eric Andre",
+        "photo": "https://img1.grunge.com/img/uploads/2017/09/what-the-world-doesn_t-know-about-eric-andre-780x438.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1
+        ]
+    },
+    {
+        "name": "Hannibal",
+        "photo": "https://img1.grunge.com/img/uploads/2017/09/what-the-world-doesn_t-know-about-eric-andre-780x438.jpg",
+        "scores": [
+            1,
+            1,
+            1,
+            1,
+            1,
+            2
+        ]
+    }
+];
+
+module.exports = friendsArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,29 @@
+const friendsData = require("../data/friends.js");
+
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
+        res.json(friendsData);
+    });
+
+    app.post("/api/friends", function (req, res) {
+        const newPerson = req.body;
+        let minimum_difference = 100;
+        let ret = friendsData[0];
+
+        for (person of friendsData) {
+            let score_difference = 0;
+            for (score in person.scores) {
+                score_difference += Math.abs(parseInt(newPerson.scores[score]) - parseInt(person.scores[score]));
+                // console.log(newPerson.scores[score], person.scores[score]);
+            }
+            if (score_difference < minimum_difference){
+                console.log('changed person', score_difference, minimum_difference)
+                minimum_difference = score_difference;
+                ret = person;
+            }
+        }
+
+        res.send(ret);
+        friendsData.push(newPerson);
+    })
+}
